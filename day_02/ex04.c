5806d7b5e9afcf50d2e66453e69e29ec804bdcd8
ayamojahid <aya.mojahid.2025@gmail.com>
2025-09-24 23:45:52 +0100

    2eme commit

--- day_02/ex04.c
+++ day_02/ex04.c
@@ -1,20 +1,22 @@
 #include <stdio.h>
 
-int main() {
-    int m1, j1, a1, m2, j2, a2;
+int main () {
+    int m, j, a;
+    int m1, j1, a1;
 
-    printf("Entrez la première date (mm/jj/aa) : ");
+    printf("Entrez la premiere date (mm/jj/aa) : ");
+    scanf("%d/%d/%d", &m, &j, &a);
+
+    printf("Entrez la deuxieme date (mm/jj/aa) : ");
     scanf("%d/%d/%d", &m1, &j1, &a1);
 
-    printf("Entrez la deuxième date (mm/jj/aa) : ");
-    scanf("%d/%d/%d", &m2, &j2, &a2);
-
-    if (a1 < a2 || (a1 == a2 && (m1 < m2 || (m1 == m2 && j1 < j2))))
-        printf("%d/%d/%02d est plus tôt que %d/%d/%02d\n", m1, j1, a1, m2, j2, a2);
-    else if (a1 == a2 && m1 == m2 && j1 == j2)
-        printf("Les deux dates sont identiques.\n");
-    else
-        printf("%d/%d/%02d est plus tôt que %d/%d/%02d\n", m2, j2, a2, m1, j1, a1);
+    if (a > a1 || (a == a1 && m > m1) || (a == a1 && m == m1 && j > j1)) {
+        printf("%d/%d/%d est plus tard que %d/%d/%d\n", m, j, a, m1, j1, a1);
+    } else if (a == a1 && m == m1 && j == j1) {
+        printf("Les deux dates sont identiques : %d/%d/%d\n", m, j, a);
+    } else {
+        printf("%d/%d/%d est plus tôt que %d/%d/%d\n", m1, j1, a1, m, j, a);
+    }
 
     return 0;
 }
